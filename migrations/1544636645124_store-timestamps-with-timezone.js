4264b1eee2d4e63b6feb43cf16821eb25f3f5a43
hcr <hcr@users.noreply.github.com>
2018-12-12 17:48:03 +0000

    fix: store timestamp with timezone

---
+++ migrations/1544636645124_store-timestamps-with-timezone.js
@@ -0,0 +1,32 @@
+exports.shorthands = undefined
+
+exports.up = pgm => {
+  pgm.dropTable('objects')
+  pgm.createTable('objects', {
+    id: {
+      type: 'serial',
+    },
+    entity_id: {
+      type: 'uuid',
+      notNull: true,
+    },
+    user_id: {
+      type: 'varchar',
+      notNull: true,
+    },
+    timestamp: {
+      type: 'timestamptz',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+    version: {
+      type: 'integer',
+      notNull: true,
+    },
+    data: {
+      type: 'jsonb',
+      notNull: true,
+    },
+  })
+  pgm.addConstraint('objects', 'OBJECTS_PRIMARY_KEY', { primaryKey: ['entity_id', 'version'] })
+}
