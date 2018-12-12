685e0b153e05a9db230ac18a2d512468ad7d3053
hcr <hcr@users.noreply.github.com>
2018-12-12 17:33:15 +0000

    refactor: renamed groupId to entityId everywhere

--- src/db.ts
+++ src/db.ts
@@ -16,33 +16,33 @@ type RetrieveBody = InsertBody & {
 
 const INSERT_QUERY = `
   INSERT INTO objects
-  (group_id, user_id, version, data)
+  (entity_id, user_id, version, data)
   values
   ($1, $2, $3, $4)
 `
 const UPDATE_QUERY = `
   INSERT INTO objects
-  (group_id, user_id, version, data)
+  (entity_id, user_id, version, data)
   values
   ($1, $2, (
     SELECT max(version)+1
     FROM objects
-    WHERE group_id=$1
+    WHERE entity_id=$1
   ), $3)
 `
 export const insert = async (body: InsertBody): Promise<RetrieveBody> => {
   try {
     const client = new Client()
     await client.connect()
-    const groupId = body.entityId || uuid.v4()
+    const entityId = body.entityId || uuid.v4()
     if (body.entityId) {
-      await client.query(UPDATE_QUERY, [groupId, body.userId, body.data])
+      await client.query(UPDATE_QUERY, [entityId, body.userId, body.data])
     } else {
       const version = 1
-      await client.query(INSERT_QUERY, [groupId, body.userId, version, body.data])
+      await client.query(INSERT_QUERY, [entityId, body.userId, version, body.data])
     }
     await client.end()
-    return await retrieve(groupId)
+    return await retrieve(entityId)
   } catch (e) {
     console.error(e)
     return Promise.reject(e)
@@ -52,11 +52,11 @@ export const insert = async (body: InsertBody): Promise<RetrieveBody> => {
 const RETRIEVE_QUERY = `
   SELECT *
   FROM objects
-  WHERE group_id=$1
+  WHERE entity_id=$1
     AND version=(
       SELECT max(version)
       FROM objects
-      WHERE group_id=$1
+      WHERE entity_id=$1
     )
 `
 export const retrieve = async (entityId: EntityId): Promise<RetrieveBody> => {
@@ -66,7 +66,7 @@ export const retrieve = async (entityId: EntityId): Promise<RetrieveBody> => {
     const result = await client.query(RETRIEVE_QUERY, [entityId])
     await client.end()
     return {
-      entityId: result.rows[0].group_id,
+      entityId: result.rows[0].entity_id,
       userId: result.rows[0].user_id,
       timestamp: result.rows[0].timestamp,
       version: result.rows[0].version,
@@ -81,7 +81,7 @@ export const retrieve = async (entityId: EntityId): Promise<RetrieveBody> => {
 const RETRIEVE_HISTORY_QUERY = `
   SELECT *
   FROM objects
-  WHERE group_id=$1
+  WHERE entity_id=$1
   ORDER BY version ASC
 `
 export const retrieveHistory = async (id: EntityId): Promise<RetrieveBody[]> => {
@@ -91,7 +91,7 @@ export const retrieveHistory = async (id: EntityId): Promise<RetrieveBody[]> => 
     const result = await client.query(RETRIEVE_HISTORY_QUERY, [id])
     await client.end()
     const objects = result.rows.map(row => ({
-      id: row.group_id,
+      id: row.entity_id,
       userId: row.user_id,
       timestamp: row.timestamp,
       version: row.version,
